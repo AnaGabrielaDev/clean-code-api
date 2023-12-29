478e6b7596e964543b400e608690879486675bcd
AnaGabriela <anagabrieladev@gmail.com>
2023-12-29 18:30:53 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from "../errors/InvalidParamError"
 import { MissingParamError } from "../errors/MissingParamError"
+import { ServerError } from "../errors/ServerError"
 import { badRequest } from "../helper/httpHelper"
 import { Controller } from "../protocols/controller"
 import { EmailValidator } from "../protocols/emailValidator"
@@ -13,21 +14,33 @@ export class SignUpController implements Controller {
 	}
 
 	handle(httpRequest: HttpRequest): HttpResponse {
-		const requiredFields = ["name", "email", "password", "passwordConfirmation"]
+		try {
+			const requiredFields = [
+				"name",
+				"email",
+				"password",
+				"passwordConfirmation",
+			]
 
-		for (const field of requiredFields) {
-			if (!httpRequest.body[field]) {
-				return badRequest(new MissingParamError(field))
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field))
+				}
 			}
-		}
 
-		if (!this.emailValidator.isValid(httpRequest.body.email)) {
-			return badRequest(new InvalidParamError("email"))
-		}
+			if (!this.emailValidator.isValid(httpRequest.body.email)) {
+				return badRequest(new InvalidParamError("email"))
+			}
 
-		return {
-			statusCode: 200,
-			body: "ok",
+			return {
+				statusCode: 200,
+				body: "ok",
+			}
+		} catch (error) {
+			return {
+				statusCode: 500,
+				body: new ServerError(),
+			}
 		}
 	}
 }
