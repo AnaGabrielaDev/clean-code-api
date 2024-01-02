173931b7e90449dac822e6426041d42da56e4573
AnaGabriela <anagabrieladev@gmail.com>
2024-01-02 00:58:59 -0300

    🔧 : disable biome's rule - noExplicitAny

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,10 +1,8 @@
 export interface HttpResponse {
 	statusCode: number
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	body: any
 }
 
 export interface HttpRequest {
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	body?: any
 }
