75c3aa911d2e83ac99ab99e99ae3a86263271469
AnaGabriela <anagabrieladev@gmail.com>
2024-01-23 14:44:40 -0300

    ♻️ refactor: create a constructor and interface to account model

--- src/domain/models/AccountModel.ts
+++ src/domain/models/AccountModel.ts
@@ -1,6 +1,20 @@
+import { randomUUID } from "node:crypto"
+interface AccountModelRequest {
+	id?: string
+	name: string
+	email: string
+	password: string
+}
 export class AccountModel {
 	id: string
 	name: string
 	email: string
 	password: string
+
+	constructor(props: AccountModelRequest) {
+		this.id = props.id ?? randomUUID().toString()
+		this.name = props.name
+		this.email = props.email
+		this.password = props.password
+	}
 }
