e32c0b4cff3816eb3988a1067d76ae9398ee1dbe
AnaGabriela <anagabrieladev@gmail.com>
2024-01-26 10:06:51 -0300

    refactor: use camelCase

--- src/main/middlewares/bodyParser.test.ts
+++ src/main/middlewares/bodyParser.test.ts
@@ -3,11 +3,11 @@ import { app } from "../config/app"
 
 describe("Body Parser Middleware", () => {
 	it("Should parse body as json", async () => {
-		app.post("/test_body_parser", (req, res) => {
+		app.post("/test/bodyParser", (req, res) => {
 			res.send(req.body)
 		})
 		await request(app)
-			.post("/test_body_parser")
+			.post("/test/bodyParser")
 			.send({ name: "Any" })
 			.expect({ name: "Any" })
 	})
